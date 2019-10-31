439e83234a911d8f19252bc681e4f324ba3976fd
half-empty <122198841@qq.com>
2019-10-31 12:36:30 +0800

    Create 20. 有效的括号.py

---
+++ 20. 有效的括号.py
@@ -0,0 +1,54 @@
+"""
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack_list = list()
+        right_set = {')', '}', ']'}
+        case_set = {'()', '{}', '[]'}
+        for c in s:
+            if c in right_set:
+                if stack_list and stack_list[-1] + c in case_set:
+                    del stack_list[-1]
+                else:
+                    return False
+            else:
+                stack_list.append(c)
+        return not stack_list
