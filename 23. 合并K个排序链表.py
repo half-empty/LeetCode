c37670f4db9f8a1ae947c9cacc214162f1ffd8de
half-empty <122198841@qq.com>
2019-10-31 14:50:00 +0800

    Create 23. 合并K个排序链表.py

---
+++ 23. 合并K个排序链表.py
@@ -0,0 +1,44 @@
+"""
+合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+
+示例:
+
+输入:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+输出: 1->1->2->3->4->4->5->6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        q = PriorityQueue()
+        res = cur = ListNode(0)
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            cur.next = node
+            cur = cur.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return res.next
