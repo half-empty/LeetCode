361e358a908d8fc1c8b2e3df909a1755ccd60f50
half-empty <122198841@qq.com>
2019-12-29 16:34:13 +0800

    2019-10-28

--- 4. 寻找两个有序数组的中位数.py
+++ 4. 寻找两个有序数组的中位数.py
@@ -17,10 +17,6 @@ nums1 = [1, 2]
 nums2 = [3, 4]
 
 则中位数是 (2 + 3)/2 = 2.5
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
 class Solution(object):
