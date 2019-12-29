28d88f4611858e38a1645c0efc91c36cf9502bd7
half-empty <122198841@qq.com>
2019-12-29 16:47:38 +0800

    2019-10-31

--- 23. 合并K个排序链表.py
+++ 23. 合并K个排序链表.py
@@ -10,10 +10,6 @@
   2->6
 ]
 输出: 1->1->2->3->4->4->5->6
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 
 # Definition for singly-linked list.
