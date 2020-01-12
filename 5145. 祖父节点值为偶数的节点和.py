bb75cf5039181499ab43e080c80e6f6d5afa862f
half-empty <122198841@qq.com>
2020-01-12 12:25:36 +0800

    2020-01-11 第 17 场双周赛 137 / 897

---
+++ 5145. 祖父节点值为偶数的节点和.py
@@ -0,0 +1,42 @@
+"""
+给你一棵二叉树，请你返回满足以下条件的所有节点的值之和：
+
+该节点的祖父节点的值为偶数。（一个节点的祖父节点是指该节点的父节点的父节点。）
+如果不存在祖父节点值为偶数的节点，那么返回 0 。
+
+ 
+
+示例：
+
+
+
+输入：root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+输出：18
+解释：图中红色节点的祖父节点的值为偶数，蓝色节点为这些红色节点的祖父节点。
+ 
+
+提示：
+
+树中节点的数目在 1 到 10^4 之间。
+每个节点的值在 1 到 100 之间。
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        ans = [0]
+        def dfs(r, f, ff):
+            if not r:
+                return
+            if ff % 2 == 0:
+                ans[0] += r.val
+            dfs(r.left, r.val, f)
+            dfs(r.right, r.val, f)
+        dfs(root, 1, 1)
+        return ans[0]
